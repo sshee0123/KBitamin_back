cfb1e921ed4c220a95000b8f7e16d9e93c48e3d3
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2022-04-24 18:35:06 +0900

    "initial back commint 220424"

---
+++ src/test/java/com/kb1/springbootback/SpringbootBackApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kb1.springbootback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
